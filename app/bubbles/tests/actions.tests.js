7297de7ab07961a99d8f5a25cc3b411bab31ffe4
ChipJuggler <scsi.rdx@gmail.com>
2016-11-23 17:03:01 +0700

    most of the app logic tests

--- app/bubbles/tests/actions.tests.js
+++ app/bubbles/tests/actions.tests.js
@@ -7,4 +7,18 @@ describe('bubbles actions', () => {
     const expectedAction = { type: constants.SET_GROUP, group };
     expect(actions.setGroup(group)).to.eql(expectedAction);
   });
+
+  it('should create action to set data', () => {
+    const data = 'data';
+    const expectedAction = { type: constants.SET_DATA, data };
+    expect(actions.setData(data)).to.eql(expectedAction);
+  });
+
+  it('should create action to set loading state', () => {
+    expect(actions.loading()).to.eql({ type: constants.LOADING });
+  });
+
+  it('should create action to set loaded state', () => {
+    expect(actions.loaded()).to.eql({ type: constants.LOADED });
+  });
 });
---
+++ app/charts/tests/util.tests.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import { constants } from '../actions';
+import { formatLabel, getMomentPeriod } from '../util/process_data';
+
+describe('charts helper functions', () => {
+  describe('formatLabel', () => {
+    it('should return properly formatted label for different values', () => {
+      expect(formatLabel(1000000000000000)).to.equal('1 PWh');
+      expect(formatLabel(1000000000000)).to.equal('1 TWh');
+      expect(formatLabel(1000000000)).to.equal('1 GWh');
+      expect(formatLabel(1000000)).to.equal('1 MWh');
+      expect(formatLabel(1000)).to.equal('1 kWh');
+      expect(formatLabel(100)).to.equal('100 Wh');
+    });
+  });
+
+  describe('getMomentPeriod', () => {
+    it('should return proper moment.js period based on resolution', () => {
+      expect(getMomentPeriod(constants.RESOLUTIONS.YEAR_MONTH)).to.equal('year');
+      expect(getMomentPeriod(constants.RESOLUTIONS.MONTH_DAY)).to.equal('month');
+      expect(getMomentPeriod(constants.RESOLUTIONS.HOUR_MINUTE)).to.equal('hour');
+      expect(getMomentPeriod(constants.RESOLUTIONS.DAY_MINUTE)).to.equal('day');
+    });
+  });
+});
