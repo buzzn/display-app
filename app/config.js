e56c3fb9f05412439b6e3e71d29ef79257eb78e5
ChipJuggler <scsi.rdx@gmail.com>
2017-03-29 00:35:59 +0700

    codever param to test deploy

--- app/config.js
+++ app/config.js
@@ -4,19 +4,16 @@ if (process.env.NODE_ENV === 'production') {
   config = {
     apiUrl: 'https://app.buzzn.net/',
     apiPath: 'api/v1',
-    codeVer: '888999',
   };
 } else if (process.env.NODE_ENV === 'staging') {
   config = {
     apiUrl: 'https://staging.buzzn.net/',
     apiPath: 'api/v1',
-    codeVer: '888999',
   };
 } else {
   config = {
     apiUrl: 'http://localhost:3000/',
     apiPath: 'api/v1',
-    codeVer: '888999',
   };
 }
 
