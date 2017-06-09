48315c1834a93250b4e2b8afb2df3bd94b4f42e2
ChipJuggler <scsi.rdx@gmail.com>
2017-06-09 10:03:58 +0300

    float autarky fix

--- app/components/group_status.js
+++ app/components/group_status.js
@@ -4,7 +4,7 @@ const GroupStatus = ({ value, text }) => (
   <div style={{ position: 'absolute', left: '30%', bottom: '40px' }}>
     <div style={{ margin: 'auto', width: '184px', height: '184px', borderRadius: '10px', backgroundColor: '#e0e0e0' }}>
       <div style={{ textAlign: 'center', paddingTop: '20px', fontSize: '70px' }}>
-        { value ? `${(value * 100).toFixed(0)}%` : 'n.a' }
+        { value ? `${Math.round(value * 100)}%` : 'n.a' }
       </div>
       <div style={{ textAlign: 'center', fontSize: '20px' }}>
         { text }
