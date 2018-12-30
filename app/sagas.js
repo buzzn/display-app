1a01914c8069f2c623699bd2405146b318604e13
ChipJuggler <scsi.rdx@gmail.com>
2018-12-30 18:49:27 +0100

    ios iframe fix

--- app/sagas.js
+++ app/sagas.js
@@ -1,13 +1,4 @@
-import {
-  put,
-  select,
-  fork,
-  spawn,
-  call,
-  take,
-  cancel,
-} from 'redux-saga/effects';
-import { delay } from 'redux-saga';
+import { put, select, fork, spawn, call, take, cancel, delay } from 'redux-saga/effects';
 import Bubbles from '@buzzn/module_bubbles';
 import { constants, actions } from './actions';
 import api from './api';
@@ -22,14 +13,11 @@ export function setScale() {
   const scaleY = window.innerHeight / 1080;
   const scale = scaleX < scaleY ? scaleX : scaleY;
   if (store) store.dispatch(actions.setWidgetScale(scale));
-  document.body.style.zoom = scale;
-  document.body.style.MozTransform = `scale(${scale})`;
+  document.body.style.transform = `scale(${scale})`;
   if (scale < 1) {
-    document.body.style.MozTransformOrigin = `${window.innerWidth -
-      1920 * scale}px 0 0`;
+    document.body.style.transformOrigin = `${window.innerWidth - 1920 * scale}px 0 0`;
   } else {
-    document.body.style.MozTransformOrigin = `${window.innerWidth -
-      1600 / scale}px 0 0`;
+    document.body.style.transformOrigin = `${window.innerWidth - 1600 / scale}px 0 0`;
   }
 }
 
@@ -39,7 +27,7 @@ export function hackScale() {
 
   window.addEventListener(
     'touchmove',
-    event => {
+    (event) => {
       if (event.scale !== 1) {
         event.preventDefault();
       }
@@ -50,7 +38,7 @@ export function hackScale() {
   let lastTouchEnd = 0;
   document.addEventListener(
     'touchend',
-    event => {
+    (event) => {
       const now = new Date().getTime();
       if (now - lastTouchEnd <= 500) {
         event.preventDefault();
@@ -77,7 +65,7 @@ export function* windowReload() {
     } catch (error) {
       logException(error);
     }
-    yield call(delay, 10 * 60 * 1000);
+    yield delay(10 * 60 * 1000);
   }
 }
 
@@ -94,8 +82,8 @@ export function* getCharts({ apiUrl, apiPath }, { groupId }) {
       logException(error);
     }
     // FIXME: temporary fix for broken charts
-    yield call(delay, 40 * 1000);
-    // yield call(delay, 15 * 60 * 1000);
+    yield delay(40 * 1000);
+    // yield delay(15 * 60 * 1000);
   }
 }
 
@@ -108,7 +96,7 @@ export function* setHealth({ apiUrl }) {
       logException(error);
       yield put(actions.setHealth({}));
     }
-    yield call(delay, 60 * 1000);
+    yield delay(60 * 1000);
   }
 }
 
@@ -152,8 +140,8 @@ export default function* appLoop() {
   }
 
   let groupId = yield call(getGroupFromUrl);
-  const metaGroup = (new URL(window.location.href)).searchParams.get('metagroup');
-  const customTitle = (new URL(window.location.href)).searchParams.get('custom-title');
+  const metaGroup = new URL(window.location.href).searchParams.get('metagroup');
+  const customTitle = new URL(window.location.href).searchParams.get('custom-title');
 
   if (!metaGroup && !groupId) return false;
 
@@ -194,11 +182,7 @@ export default function* appLoop() {
       if (group.id) {
         yield put(actions.setGroup(group));
         yield put(actions.loadedGroup());
-        const chartSaga = yield fork(
-          getCharts,
-          { apiUrl, apiPath },
-          { groupId },
-        );
+        const chartSaga = yield fork(getCharts, { apiUrl, apiPath }, { groupId });
         yield put(Bubbles.actions.setGroupId(groupId));
 
         yield take(constants.CANCEL);
@@ -207,7 +191,7 @@ export default function* appLoop() {
         yield cancel(chartSaga);
         yield put(Bubbles.actions.stopRequests());
       } else {
-        yield call(delay, 10 * 60 * 1000);
+        yield delay(10 * 60 * 1000);
       }
     } catch (error) {
       if (error._status === 404 || error._status === 403) {
@@ -217,7 +201,7 @@ export default function* appLoop() {
       }
       // FIXME: temporary hack, please change to proper err code handling
       yield put(actions.loadedGroup());
-      yield call(delay, 10 * 60 * 1000);
+      yield delay(10 * 60 * 1000);
     }
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -32,30 +32,6 @@ module.exports = {
         test: /\.(ts|tsx|js)$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        query: {
-          presets: [
-            [
-              '@babel/env',
-              {
-                targets: {
-                  browsers: ['last 2 versions', 'safari >= 7'],
-                  modules: false,
-                  debug: true,
-                },
-              },
-            ],
-            '@babel/stage-3',
-            '@babel/react',
-            '@babel/typescript',
-          ],
-          plugins: [
-            'react-hot-loader/babel',
-            '@babel/plugin-proposal-object-rest-spread',
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-syntax-class-properties',
-            '@babel/plugin-syntax-object-rest-spread',
-          ],
-        },
       },
       {
         test: /\.css$/,
